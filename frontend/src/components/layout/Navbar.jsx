e6447656ba37aa523ff3c9529938ba1f871f032b
Himanshu Singh <131706067+clay108@users.noreply.github.com>
2024-11-15 14:01:56 +0530

    Update Navbar.jsx

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
     >
       <nav>
         <div className="logo">
-          Zeta<span>Blog</span>
+          Story<span>Nest</span>
         </div>
         <div className={show ? "links show" : "links"}>
           <ul>
